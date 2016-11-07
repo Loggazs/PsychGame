a70aac9666ab20fbdc95c43998b90ba55d3f256d
Lukas <lkemper@mail.upb.de>
2016-11-07 17:08:06 +0100

    these i need

---
+++ PsychGame/PsyychGame2/Source/PsyychGame2/PsyychGame2.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PsyychGame2.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PsyychGame2, "PsyychGame2" );
